c44f8d1ee5587c9bc7769fb8fe69905639b7d533
MikusR <mikus.rakis@gmail.com>
2024-02-24 19:30:35 +0200

    view+article

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+<article class="max-w-2xl mx-auto p-6">
+    <h1 class="text-3xl font-bold mb-4">Article Title</h1>
+    <p class="text-gray-600 mb-4">Published on January 1, 2022 by John Doe</p>
+    <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...</p>
+    <a href="/post" class="text-blue-500 hover:underline">Read more</a>
+
+</article>
+<article class="max-w-2xl mx-auto p-6">
+    <h1 class="text-3xl font-bold mb-4">Article Title</h1>
+    <p class="text-gray-600 mb-4">Published on January 1, 2022 by John Doe</p>
+    <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...</p>
+    <a href="/post" class="text-blue-500 hover:underline">Read more</a>
+
+</article>
+<article class="max-w-2xl mx-auto p-6">
+    <h1 class="text-3xl font-bold mb-4">Article Title</h1>
+    <p class="text-gray-600 mb-4">Published on January 1, 2022 by John Doe</p>
+    <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...</p>
+    <a href="/post" class="text-blue-500 hover:underline">Read more</a>
+
+</article>
+<article class="max-w-2xl mx-auto p-6">
+    <h1 class="text-3xl font-bold mb-4">Article Title</h1>
+    <p class="text-gray-600 mb-4">Published on January 1, 2022 by John Doe</p>
+    <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...</p>
+    <a href="/post" class="text-blue-500 hover:underline">Read more</a>
+
+</article>
+</body>
+</html>
