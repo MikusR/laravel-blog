578f16874ba8dbdd898c60ccf40c5cd5a57edc1a
MikusR <mikus.rakis@gmail.com>
2024-02-26 13:48:45 +0200

    refactor

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,12 +6,9 @@ class Post
 {
     public function find($slug)
     {
-
-
-        if (!file_exists($path = resource_path() . "/posts/{$slug}.html")) {
+        if (!file_exists($path = resource_path("posts/{$slug}.html"))) {
             return redirect('/');
         }
         return cache()->remember("posts.{$slug}", 5, fn() => file_get_contents($path));
-
     }
 }
