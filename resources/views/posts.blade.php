e5e058bbb7538f5cb725468a3ecd7e302395a191
MikusR <mikus.rakis@gmail.com>
2024-02-26 19:24:12 +0200

    refactor

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -15,8 +15,9 @@
        class="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900">Posts</a>
 
 </nav>
+
 @foreach($posts as $post)
-    <article class="max-w-2xl mx-auto p-6">
+    <article class="{{ $loop->even ? 'bg-slate-50 ' : '' }}max-w-2xl mx-auto p-6">
         <h1 class="text-3xl font-bold mb-4">{{ $post->title }}</h1>
         <p class="text-gray-600 mb-4">{{$post->date}} by {{ $post->author }}</p>
         {!! $post->body !!}
