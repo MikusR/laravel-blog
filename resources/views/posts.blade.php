f5f522f24c85c0e1d936a446abca7fc1ff936637
MikusR <mikus.rakis@gmail.com>
2024-03-17 17:47:39 +0200

    Str::limit($post->body,150)

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,7 +4,7 @@
         <article class="{{ $loop->even ? 'bg-slate-50 ' : '' }}max-w-2xl mx-auto p-6">
             <h1 class="text-3xl font-bold mb-4">{{ $post->title }}</h1>
             <p class="text-gray-600 mb-4">{{$post->date}} by {{ $post->author }}</p>
-            {!! $post->body !!}
+            {!! Str::limit($post->body,150) !!}
             <a href="/posts/{{$post->id}}"
                class="text-blue-500 hover:underline">Read
                 more</a>
